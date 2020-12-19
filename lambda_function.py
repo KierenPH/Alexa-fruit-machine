ab444bc2288340bfb73d6c77b540c5832f3b0c5c
Kieren <kieren@kieren.uk>
2020-12-19 01:26:46 +0000

    Using attributes to store values on ASK also removing other files used to access firebase not needed anymore.

--- lambda_function.py
+++ lambda_function.py
@@ -1,41 +1,78 @@
 from __future__ import print_function
 import gamefiles
-import database
-
-Money = float(1.0)
 
 
 def lambda_handler(event, context):
     if event['request']['type'] == "LaunchRequest":
-        return on_launch(event['request'], event['session'])
+        return on_launch(event)
     elif event['request']['type'] == "IntentRequest":
         return on_intent(event)
     elif event['request']['type'] == "SessionEndedRequest":
-        return on_session_ended(event['request'], event['session'])
+        session_attributes = {
+            "Money": 0
+        }
+        card_title = "Game End"
+        speech_output = "Thanks for playing fruit machine, if you'd like to play again. Say, Launch Fruit Machine."
+        reprompt_text = speech_output
+        should_end_session = True
+        return build_response(session_attributes,
+                              build_speechlet_response(card_title, speech_output, reprompt_text, should_end_session))
 
 
 def on_intent(event):
-    if event["request"]["intent"]["name"] == "roll":
-        tempmoney, Message = gamefiles.Game(Money)
-        Message = Message + " You have £" + str(tempmoney) + " left."
-        session_attributes = {}
-        card_title = "Roll Complete"
-        speech_output = Message
+    if event["request"]["intent"]["name"] == "AMAZON.StopIntent":
+        session_attributes = {
+            "Money": 0
+        }
+        card_title = "Game End"
+        speech_output = "Thanks for playing fruit machine, if you'd like to play again. Say, Launch Fruit Machine."
         reprompt_text = speech_output
-        should_end_session = False
+        should_end_session = True
         return build_response(session_attributes,
                               build_speechlet_response(card_title, speech_output, reprompt_text, should_end_session))
+    if event["request"]["intent"]["name"] == "roll":
+        Money = getmoney(event)
+        tempmoney, Message = gamefiles.Game(Money)
+        tempmoney = round(tempmoney, 2)
+        if tempmoney <= 0:
+            session_attributes = {
+                "Money": 0
+            }
+            card_title = "Ran out of money"
+            speech_output = "You have ran out of money, unfortunatly this means you've lost. If you'd like to launch the game again say, Launch Fruit Machine."
+            reprompt_text = speech_output
+            should_end_session = True
+            return build_response(session_attributes, build_speechlet_response(card_title, speech_output, reprompt_text,
+                                                                               should_end_session))
+        else:
+            Message = Message + " You have £" + str(tempmoney) + " left."
+            session_attributes = {
+                "Money": tempmoney
+            }
+            card_title = "Roll Complete"
+            speech_output = Message
+            reprompt_text = speech_output
+            should_end_session = False
+            return build_response(session_attributes, build_speechlet_response(card_title, speech_output, reprompt_text,
+                                                                               should_end_session))
 
 
 def on_session_started(session_started_request, session):
     """ Called when the session starts """
 
-    print("on_session_started requestId=" + session_started_request['requestId']
-          + ", sessionId=" + session['sessionId'])
+    print(
+        "on_session_started requestId=" + session_started_request['requestId'] + ", sessionId=" + session['sessionId'])
 
 
-def on_launch(launch_request, session):
-    return get_welcome_response()
+def on_launch(event):
+    event["session"]["attributes"] = {}
+    Money = 1.0
+    return get_welcome_response(event)
+
+
+def getmoney(event):
+    Money = event["session"]["attributes"]["Money"]
+    return Money
 
 
 def on_session_ended(session_ended_request, session):
@@ -43,11 +80,12 @@ def on_session_ended(session_ended_request, session):
           ", sessionId=" + session['sessionId'])
 
 
-def get_welcome_response():
-    temp = str(Money)
-    session_attributes = {}
+def get_welcome_response(event):
+    session_attributes = {
+        "Money": 1.0
+    }
     card_title = "Welcome"
-    speech_output = "Welcome to the fruit machine test game. you currently have £" + temp + " if you would like to play say Roll else you can quit or say help."
+    speech_output = "Welcome to the fruit machine test game. you currently have £1 if you would like to play say Roll else you can quit or say help."
     reprompt_text = speech_output
     should_end_session = False
     return build_response(session_attributes, build_speechlet_response(
